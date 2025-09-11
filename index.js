e619d93bd57ea6ed56fb6ce6a0c2f4e23b9d418d
abhayabikramshahi <abhayabikramshahiofficial@gmail.com>
2025-09-11 21:50:04 +0545

    add

--- index.js
+++
@@ -1,9 +0,0 @@
-// Import CSS directly
-import "./index.css"; // or "./dist/index.css" if you put it in dist
-
-// Optional JS components
-export const Nep = () => { console.log("Nep component"); };
-export const Button = () => { console.log("Button component"); };
-export const Card = () => { console.log("Card component"); };
-export const Font = () => { console.log("Font component"); };
-export const Responsive = () => { console.log("Responsive component"); };
